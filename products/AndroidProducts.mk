e211a186ed245307f0fe681306f11b64456454b3
adi153 <singhmanohar6699@gmail.com>
2021-05-02 03:57:22 +0000

    Bringup rolex
    Signed-off-by: adi153 <singhmanohar6699@gmail.com>
    Change-Id: I5a455c1a8138375f3e00f679d3a5d364b0f4e5fe

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -38,6 +38,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus8t/pa_oneplus8t.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/raphael/pa_raphael.mk \
+    $(LOCAL_DIR)/rolex/pa_rolex.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
     $(LOCAL_DIR)/wayne/pa_wayne.mk \
     $(LOCAL_DIR)/X00TD/pa_X00TD.mk
@@ -66,6 +67,7 @@ COMMON_LUNCH_CHOICES += \
     pa_oneplus8t-userdebug \
     pa_polaris-userdebug \
     pa_raphael-userdebug \
+    pa_rolex-userdebug \
     pa_sagit-userdebug \
     pa_wayne-userdebug \
     pa_X00TD-userdebug
